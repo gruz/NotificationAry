1e2b5562e035cc350db5e4db2d42c4aa058a4efd
Gruz <arygroup@gmail.com>
2018-04-07 19:36:26 +0300

    0.2.17 alpha build 33

--- scriptary.php
+++ scriptary.php
@@ -261,16 +261,16 @@ class ScriptAry
 
 		$success = true;
 
-		static $first = 'first';
-		
-		file_put_contents('plugin.txt', print_r($plugin, true));
-		file_put_contents($first . '.txt', $pluginIsPublished . '|' . $state);
-		file_put_contents(uniqid() . '.txt', $pluginIsPublished . '|' . $state);
-		$first = 'second';
+// static $first = 'first';
+
+// file_put_contents('plugin.txt', print_r($plugin, true));
+// file_put_contents($first . '.txt', $pluginIsPublished . '|' . $state);
+// file_put_contents(uniqid() . '.txt', $pluginIsPublished . '|' . $state);
+// $first = 'second';
 
 		if (($pluginIsPublished && 1 === $state) || (!$pluginIsPublished && 1 !== $state))
 		{ 
-			return;
+			// return;
 		}
 
 
