32b52fbff07d22c3437084e905ef58cee79bdb31
Gruz <arygroup@gmail.com>
2018-04-07 15:15:18 +0300

    0.2.17 alpha build 10

--- scriptary.php
+++ scriptary.php
@@ -246,12 +246,12 @@ class ScriptAry
 	 *
 	 * @return   void
 	 */
-	public function _publishPlugin($plg_name,$plg_type, $plg_full_name = null, $state = 1)
+	public function _publishPlugin($plg_name, $plg_type, $plg_full_name = null, $state = 1)
 	{
 		$plugin = JPluginHelper::getPlugin($plg_type, $plg_name);
 		$success = true;
 
-		if (empty($plugin))
+		if (empty($plugin) || ! $state )
 		{
 			// Get the smallest order value
 			$db = jfactory::getdbo();
