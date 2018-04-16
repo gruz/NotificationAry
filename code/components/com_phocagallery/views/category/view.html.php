1f496dbde39302292c147535e6f294533e074677
Gruz <arygroup@gmail.com>
2018-04-17 02:28:05 +0300

    Prepares to run onContentAfterSave for PG multiple upload

---
+++ code/components/com_phocagallery/views/category/view.html.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * @package		Joomla.Framework
+ * @copyright	Copyright (C) Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @component Phoca Component
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License version 2 or later;
+ */
+
+ 
+
+class PhocaGalleryViewCategory extends PhocaGalleryViewCategoryDefault
+{
+	function display($tpl = null)
+	{
+
+		$app = JFactory::getApplication();
+		$muFailed = $app->input->get( 'mufailed', '0', 'int' );
+		$muUploaded = $app->input->get( 'muuploaded', '0', 'int' );
+
+		if ($muUploaded > 0) {
+			$key       = 'uploadedImages';
+			$namespace = 'NotificationAry.PhocaGalleryMultipleUpload';
+			
+			// TODO Here prepare for onContentAfterSave
+			$session           = \JFactory::getSession();
+			$uploadedImages = $session->get($key, [], $namespace);
+			$uploadedImages = $session->set($key, $uploadedImages, $namespace);
+
+			\JEventDispatcher::getInstance()->trigger(
+				'onContentAfterSave',
+				array(
+					$context,
+					$contentItem,
+					$isNew
+				)
+			);
+		}
+
+	
+	
+		return parent::display($tpl);
+	}
+}
